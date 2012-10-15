d099a00a1f48b71469295a75df346388090cbdad
Jack Deeth <JackDeeth@gmail.com>
2012-10-15 21:39:59 +1100

    refactored, doensn't work?

--- AnnunciatorDev.ino
+++ AnnunciatorDev.ino
@@ -1,42 +1,41 @@
 #include <Bounce.h>
 
-enum inPins {
+enum {
   PIN_SW1 = 0,
   PIN_SW2 = 4,
   PIN_SW3 = 8,
   PIN_SW4 = 12,
-  PIN_SW5 = 16 };
-  
-Bounce sw1 = Bounce (PIN_SW1, 5);
-Bounce sw2 = Bounce (PIN_SW2, 5);
-Bounce sw3 = Bounce (PIN_SW3, 5);
-Bounce sw4 = Bounce (PIN_SW4, 5);
-Bounce sw5 = Bounce (PIN_SW5, 5);
+  PIN_SW5 = 16,
+  PIN_COUNT = 5};
+
+const int InPin[] = {
+  PIN_SW1, PIN_SW2, PIN_SW3, PIN_SW4, PIN_SW5};
+
+Bounce * sw[5];
 
 void setup() {
-  pinMode(PIN_SW1, INPUT_PULLUP);
-  pinMode(PIN_SW2, INPUT_PULLUP);
-  pinMode(PIN_SW3, INPUT_PULLUP);
-  pinMode(PIN_SW4, INPUT_PULLUP);
-  pinMode(PIN_SW5, INPUT_PULLUP);
-  
+  for (int i = 0; i < PIN_COUNT; ++i) {
+    sw[i] = new Bounce (InPin[i], 5);
+    pinMode(InPin[i], INPUT_PULLUP );
+  }
+
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
-  sw1.update();
-  sw2.update();
-  sw3.update();
-  sw4.update();
-  sw5.update();
-  
-  if(sw1.read() == LOW
-  || sw2.read() == LOW
-  || sw3.read() == LOW
-  || sw4.read() == LOW
-  || sw5.read() == LOW) {
+  for (int i = 0; i < PIN_COUNT; ++i) {
+    sw[i]->update();
+  }
+
+  if(sw[1]->read() == LOW
+    || sw[2]->read() == LOW
+    || sw[3]->read() == LOW
+    || sw[4]->read() == LOW
+    || sw[5]->read() == LOW) {
     digitalWrite(LED_BUILTIN, HIGH);
-  } else {
+  } 
+  else {
     digitalWrite(LED_BUILTIN, LOW);
   }
 }
+
