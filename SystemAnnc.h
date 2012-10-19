6a9ea612a678fff1ce5e2f1a260eaa5a59e7b2b2
Jack Deeth <JackDeeth@gmail.com>
2012-10-19 11:53:09 +1100

    Included sometimes-necessary Arduino headers and LGPL licence

--- SystemAnnc.h
+++ SystemAnnc.h
@@ -1,11 +1,37 @@
 #ifndef SystemAnnc_h
 #define SystemAnnc_h
 
+#if ARDUINO >= 100
+#include "Arduino.h"
+#else
+#include "WProgram.h"
+#endif
+
+
 //////////////////////////// SystemAnnc class /////////////////////////////////
 //
 // This class represents an individual System Annunciator on the glareshield of
 // the Boeing 737-NG.
 //
+// Copyright 2012 Jack Deeth
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// I would appreciate, but not insist, on attribution if this code is
+// incorporated into other projects.
+//
+
 
 // Maximum sub-annunciators to be grouped with any one System Annunciator
 const int MAX_ANNCS_PER_SYSTEM = 12;
